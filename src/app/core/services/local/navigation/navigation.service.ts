49673414acc62da302f5213e288cb2db228d76eb
pcboby <pcboby@126.com>
2020-03-21 16:04:39 +0800

    update

--- src/app/core/services/local/navigation/navigation.service.ts
+++ src/app/core/services/local/navigation/navigation.service.ts
@@ -46,7 +46,7 @@ export class NavigationService {
   }
 
   // 主选中
-  selected_master = 'Home'; // Home | APicture;
+  selected_master = 'FloodDispatch'; // Home | APicture;
   // 次选中
   selected_sub: any;
 
